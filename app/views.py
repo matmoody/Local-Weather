65e2e61e0e5e304377e9621ea25b527f4601bec3
Matt Moody <moody47@gmail.com>
2014-03-07 13:52:44 -0600

    Large round of updates

--- app/views.py
+++ app/views.py
@@ -7,15 +7,21 @@ from keys import api_key
 lat = 38.8242
 lng = -97.6072
 
+
+
 forecast = forecastio.load_forecast(api_key, lat, lng)
 
 byHour = forecast.hourly()
+byMinute = forecast.minutely()
 now = forecast.currently()
+day = forecast.daily()
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template("index.html",
+	return render_template("base.html",
 		title = 'Home',
 		now = forecast.currently(),
-		byHour = forecast.hourly())
+		byHour = forecast.hourly(),
+		byMinute = forecast.minutely(),
+		day = forecast.daily())

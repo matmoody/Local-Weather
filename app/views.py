228ab5dc3814a8b2167ccd13c1b7ce9387f2dbb3
Matt Moody <moody47@gmail.com>
2014-03-14 16:40:08 -0500

    Fixed deletion

---
+++ app/views.py
@@ -0,0 +1,27 @@
+from flask import render_template
+from app import app
+import forecastio
+from keys import api_key
+
+
+lat = 38.840280
+lng = -97.611424
+
+
+
+forecast = forecastio.load_forecast(api_key, lat, lng)
+
+byHour = forecast.hourly()
+byMinute = forecast.minutely()
+now = forecast.currently()
+day = forecast.daily()
+
+@app.route('/')
+@app.route('/index')
+def index():
+	return render_template("base.html",
+		title = 'Home',
+		now = forecast.currently(),
+		byHour = forecast.hourly(),
+		byMinute = forecast.minutely(),
+		day = forecast.daily())
